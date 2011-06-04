293439474f9f76d38149733c102b56b197850229
Satoshi Ohki <roothybrid7@gmail.com>
2011-06-05 00:26:44 +0900

    integrate per_page partial view

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,4 +1,8 @@
 module GroupsHelper
+  def link_to_items_per_page
+    lambda {|count| link_to(count, count.to_i == 0 ? groups_path : groups_path(:per_page => count.to_i)) }
+  end
+
 #  def link_to_add_fields(name, f, association)
 #    @group.servers.build
 #logger.debug(f.object.servers)
--- app/helpers/zones_helper.rb
+++ app/helpers/zones_helper.rb
@@ -1,4 +1,16 @@
 module ZonesHelper
+  def link_to_items_per_page
+    lambda do |count|
+      link_to(count, count.to_i == 0 ? zones_path : zones_path(:per_page => count.to_i))
+    end
+  end
+
+  def link_to_items_per_page_for_records(zone)
+    lambda do |count|
+      link_to(count, count.to_i == 0 ? all_records_zone_path : all_records_zone_path(zone, :per_page => count.to_i))
+    end
+  end
+
   def parse_rdata(rdata)
     if rdata.is_a?(Array)
       rdata.map {|d| "#{d}" }.join(' ')
