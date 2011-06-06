35c7f5b8faa27b092720445f063fa34a7a61bad4
Satoshi Ohki <roothybrid7@gmail.com>
2011-06-06 19:49:40 +0900

    release version: v0.1.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,11 @@ BeagleNsupdate::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'} #FIXME: TEMPORARY SETTINGS
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => 'utf-8'
 
   # Enable threaded mode
   # config.threadsafe!
