44aec98137f4711f0cb87858f584e19f0642739e
Satoshi Ohki <roothybrid7@gmail.com>
2011-06-10 12:21:25 +0900

    mistake default url settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ BeagleNsupdate::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => ENV['HTTP_HOST'] || 'localhost:3000'} #FIXME: TEMPORARY SETTINGS
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'} #FIXME: TEMPORARY SETTINGS
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
